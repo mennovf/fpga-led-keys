6ef26ed6763988614a415cf0acb56cd10981b174
mennovf <mennovanfrachem@hotmail.com>
2023-04-17 00:12:02 +0200

    Add ledid calculation

--- Scripts/detectors.py
+++ Scripts/detectors.py
@@ -1,9 +1,15 @@
 import math
+import sys
 
-def sea(f):
-    for i in range(8):
-        p = 2 ** i
-        nearest = round(f / p)
+LED_SPACING = 0.71
+WBR = 2.6
+BRW = 0.8
+WW = 2.25
+WBL = 0.864
+BLW = 1.296
+WBC = WW / 2
+BCW = WBC
+KEY_SPACINGS = (WBR, BRW, WW, WBL, BLW, WBR, BRW, WW, WBL, BLW, WBC, BCW)
 
 A4 = 440
 LOWEST_FREQUENCIES = [440/(2**4)*(2**(i/12)) for i in range(12)]
@@ -16,7 +22,6 @@ FS = 2500000 / 64
 THR_DELTAF_0P8 = 14.2 * 1000
 
 parameters = []
-
 for fi, f in enumerate(FREQUENCIES):
     fneighbour = FREQUENCIES[fi - 1] if fi >= 1 else FREQUENCIES[1]
     deltaf = abs(fneighbour - f)
@@ -36,6 +41,11 @@ for fi, f in enumerate(FREQUENCIES):
 CHUNKN = 11
 THRESHOLD = 0.05
 start = True
+running_key_distance = -KEY_SPACINGS[0]
+
+seen = set()
+duplicates = []
+
 print('(', end='')
 for chunki, chunk in enumerate(parameters[i:i+CHUNKN] for i in range(0, len(parameters), CHUNKN)):
     if start:
@@ -46,7 +56,13 @@ for chunki, chunk in enumerate(parameters[i:i+CHUNKN] for i in range(0, len(para
 
     cstart = True
     for i, (k, N) in enumerate(chunk):
-        s = f'(k => {k}, N => {N}, ledid => {chunki*CHUNKN + i}, threshold => to_sfixed({THRESHOLD}, MULTIPLIER_LEFT, MULTIPLIER_RIGHT))'
+        index = chunki*CHUNKN + i
+        running_key_distance += KEY_SPACINGS[index % len(KEY_SPACINGS)]
+        ledid = round(running_key_distance / LED_SPACING)
+        if ledid in seen:
+            duplicates.append(ledid)
+        seen.add(ledid)
+        s = f'(k => {k}, N => {N}, ledid => {ledid}, threshold => to_sfixed({THRESHOLD}, MULTIPLIER_LEFT, MULTIPLIER_RIGHT))'
         if cstart:
             cstart = False
         else:
@@ -54,5 +70,9 @@ for chunki, chunk in enumerate(parameters[i:i+CHUNKN] for i in range(0, len(para
         print(s, end='', sep='')
     print(')', end='')
 print(')')
+
+if duplicates:
+    print(f'Error: duplicate LED IDs={duplicates}', file=sys.stderr)
+    sys.exit(-1)
 
 
